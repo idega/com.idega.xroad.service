f1fe8aff4b7f1005fe2b38869e3f41b5c9a200f9
lapiukshtiss <martynas.stake@gmail.com>
2013-05-03 15:27:32 +0300

    Implementation of "getMessagesList"

--- src/java/com/idega/xroad/service/wsdl/EhubserviceServiceSkeleton.java
+++ src/java/com/idega/xroad/service/wsdl/EhubserviceServiceSkeleton.java
@@ -6,13 +6,38 @@
  */
 package com.idega.xroad.service.wsdl;
 
+import is.idega.idegaweb.egov.message.business.CommuneMessageBusiness;
+import is.idega.idegaweb.egov.message.data.UserMessage;
+
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ejb.FinderException;
+
+import net.x_rd.ee.ehubservice.producer.GetMessagesList;
+import net.x_rd.ee.ehubservice.producer.GetMessagesListE;
+import net.x_rd.ee.ehubservice.producer.GetMessagesListResponse;
+import net.x_rd.ee.ehubservice.producer.GetMessagesListResponseE;
 import net.x_rd.ee.ehubservice.producer.GetServiceList;
 import net.x_rd.ee.ehubservice.producer.GetServiceListE;
 import net.x_rd.ee.ehubservice.producer.GetServiceListResponse;
 import net.x_rd.ee.ehubservice.producer.GetServiceListResponseE;
+import net.x_rd.ee.ehubservice.producer.LabelType;
+import net.x_rd.ee.ehubservice.producer.LangType;
+import net.x_rd.ee.ehubservice.producer.Message_type0;
+import net.x_rd.ee.ehubservice.producer.Request_type8;
 import net.x_rd.ee.ehubservice.producer.Request_type9;
 import net.x_rd.ee.ehubservice.producer.Response_type10;
+import net.x_rd.ee.ehubservice.producer.Response_type5;
 
+import com.idega.business.IBOLookup;
+import com.idega.business.IBOLookupException;
+import com.idega.user.business.UserBusiness;
+import com.idega.user.data.User;
+import com.idega.util.CoreUtil;
+import com.idega.util.ListUtil;
 import com.idega.util.StringUtil;
 
 /**
@@ -28,6 +53,13 @@ import com.idega.util.StringUtil;
  */
 public class EhubserviceServiceSkeleton implements
 		EhubserviceServiceSkeletonInterface {
+	
+	protected static final Logger LOGGER = Logger.getLogger(
+			EhubserviceServiceSkeleton.class.getName());
+	
+	private CommuneMessageBusiness communeMessageBusiness;
+	
+	private UserBusiness userBusiness = null;
 	
 	/**
 	 * Auto generated method signature
@@ -191,19 +223,83 @@ public class EhubserviceServiceSkeleton implements
 
 		return serviceListResponseE;
 	}
-
+	
 	/**
-	 * Auto generated method signature
 	 * 
-	 * @param getMessagesList110
-	 * @return getMessagesListResponse117
+	 * <p>Searches data source for available messages by given 
+	 * service provider id and {@link User} personal id.</p>
+	 * @param messagesListRequestE - request form service, which contains 
+	 * provider id and {@link User} personal ID, not <code>null</code>;
+	 * @return response filled with data from {@link Collection} of 
+	 * {@link UserMessage}s.
+	 * @throws NullPointerException when service provider id, 
+	 * {@link User} personal id not given or nothing found by given credentials.
+	 * @author <a href="mailto:martynas@idega.com">Martynas Stakė</a>
 	 */
-
-	public net.x_rd.ee.ehubservice.producer.GetMessagesListResponseE getMessagesList(
-			net.x_rd.ee.ehubservice.producer.GetMessagesListE getMessagesList110) {
-		// TODO : fill this with the necessary business logic
-		throw new java.lang.UnsupportedOperationException("Please implement "
-				+ this.getClass().getName() + "#getMessagesList");
+	public GetMessagesListResponseE getMessagesList(GetMessagesListE messagesListRequestE) {
+		if (messagesListRequestE == null) {
+			throw new NullPointerException(GetMessagesListE.class.getName() + 
+					" is null. Please provide correct request!");
+		}
+		
+		GetMessagesList messagesListRequest = messagesListRequestE.getGetMessagesList();
+		if (messagesListRequest == null) {
+			throw new NullPointerException(GetMessagesList.class.getName() + 
+					" is null. Please provide correct request!");
+		}
+		
+		Request_type8 request = messagesListRequest.getRequest();
+		if (request == null) {
+			throw new NullPointerException(Request_type8.class.getName() + 
+					" is null. Please provide correct request!");
+		}
+		
+		String serviceProviderID = request.getServiceProviderId();
+		if (StringUtil.isEmpty(serviceProviderID)) {
+			throw new NullPointerException("Service provider ID " + 
+					"is null. Please provide correct service provider id!");
+		}
+		
+		String personalID = request.getCitizenId();
+		if (StringUtil.isEmpty(personalID)) {
+			throw new NullPointerException("Personal ID of " + User.class + 
+					"is null. Please provide correct personal id!");
+		}
+		
+		Collection<UserMessage> messages = null;
+		try {
+			messages = getCommuneMessageBusiness().findMessages(getUser(personalID));
+		} catch (Exception e) {
+			LOGGER.log(Level.WARNING, "Unable to find " + 
+					UserMessage.class.getName() + " for " + User.class + 
+					" by id: " + personalID, e);
+		}
+		
+		if (ListUtil.isEmpty(messages)) {
+			throw new NullPointerException("No messages found for " + 
+					User.class.getName() + " by personal id: " + personalID);
+		}
+		
+		Response_type5 response = new Response_type5();
+		for (UserMessage message : messages) {
+			Message_type0 messageType = new Message_type0();
+			messageType.setBody(message.getBody());
+			messageType.setDate(message.getCreated());
+			messageType.setId(message.getId());
+			messageType.setOfficial(message.getOwner().getName());
+			messageType.setOrgLabel(getLabelType(message.getSubject()));
+			messageType.setSubject(message.getSubject());
+			
+			response.addMessage(messageType);
+		}
+		
+		GetMessagesListResponse messagesListResponse = new GetMessagesListResponse();
+		messagesListResponse.setResponse(response);
+		
+		GetMessagesListResponseE messagesListResponseE = new GetMessagesListResponseE();
+		messagesListResponseE.setGetMessagesListResponse(messagesListResponse);
+		
+		return messagesListResponseE;
 	}
 
 	/**
@@ -232,5 +328,69 @@ public class EhubserviceServiceSkeleton implements
 		// TODO : fill this with the necessary business logic
 		throw new java.lang.UnsupportedOperationException("Please implement "
 				+ this.getClass().getName() + "#getPrefilledDocument");
+	}
+	
+	protected CommuneMessageBusiness getCommuneMessageBusiness() {
+		if (this.communeMessageBusiness != null) {
+			return this.communeMessageBusiness;
+		}
+		
+		try {
+			this.communeMessageBusiness = IBOLookup.getServiceInstance(
+					CoreUtil.getIWContext(), CommuneMessageBusiness.class);
+		} catch (IBOLookupException e) {
+			LOGGER.log(Level.WARNING, 
+					"Unable to get: " + CommuneMessageBusiness.class + ": ", e);
+		}
+		
+		return this.communeMessageBusiness;
+	}
+	
+	protected UserBusiness getUserBusiness() {
+		if (this.userBusiness != null) {
+			return this.userBusiness;
+		}
+
+		try {
+			this.userBusiness = IBOLookup.getServiceInstance(
+					CoreUtil.getIWContext(), UserBusiness.class);
+		} catch (IBOLookupException e) {
+			LOGGER.log(Level.WARNING, 
+					"Unable to get: " + UserBusiness.class + ": ", e);
+		}
+
+		return this.userBusiness;
+	}
+	
+	protected User getUser(String personalId) {
+		if (StringUtil.isEmpty(personalId)) {
+			return null;
+		}
+		
+		try {
+			return getUserBusiness().getUser(personalId);
+		} catch (RemoteException e) {
+			LOGGER.log(Level.WARNING, "Unable to connect database: ", e);
+		} catch (FinderException e) {
+			LOGGER.log(Level.WARNING, "Unable to find " + User.class + 
+					" by personal id: " + personalId);
+		}
+		
+		return null;
+	}
+	
+	protected LabelType getLabelType(String label) {
+		if (StringUtil.isEmpty(label)) {
+			return null;
+		}
+		
+		LabelType labelType = new LabelType();
+		labelType.setText(label);
+		
+		LangType langType = new LangType();
+		langType.setLangType(CoreUtil.getCurrentLocale().getLanguage());
+		labelType.setLang(langType);
+		
+		return labelType;
 	}
 }
